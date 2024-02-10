c842ef7ca17404909a185e8f95ec021766cc2d3a
araniddd <emailfordevops@gmail.com>
2024-02-10 19:03:12 +0000

    bash files

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+echo Hello world >> kaizen
+mkdir hello
+touch world
+useradd tim
+chmod 000 hello
+chown tim world
---
+++ script2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+
+touch file1
+useradd brad
+chown brat file1
+
+touch file2
+useradd ann
+chown ann file2
+ 
+# I'm installing packages
+
+
+
+yum install wget -y
